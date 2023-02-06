f2314a7f86652b97032c60c50bb111970cf11a07
kroukS <113801605+KrouksCommunity@users.noreply.github.com>
2023-02-07 01:53:31 +0200

    Add files via upload

---
+++ test222.py
@@ -0,0 +1,45 @@
+import subprocess
+import re
+
+def MonitorMode(interface, state):
+    if state == "on":
+        subprocess.run(["sudo", "ifconfig", interface, "down"])
+        subprocess.run(["sudo", "iwconfig", interface, "mode", "monitor"])
+        subprocess.run(["sudo", "ifconfig", interface, "up"])
+        print("[+] Monitor mode enabled")
+    elif state == "off":
+        subprocess.run(["sudo", "ifconfig", interface, "down"])
+        subprocess.run(["sudo", "iwconfig", interface, "mode", "managed"])
+        subprocess.run(["sudo", "ifconfig", interface, "up"])
+        print("[-] Monitor mode disabled")
+
+def ListInterfaces():
+    result = subprocess.run(["ifconfig"], stdout=subprocess.PIPE)
+    output = result.stdout.decode("utf-8")
+    interfaces = re.findall(r'^(\w+)\s', output, re.MULTILINE)
+    print("[+] List of network interfaces:")
+    for i in interfaces:
+        print(i)
+
+def main():
+    while True:
+        print("1. Monitor mode (on/off)")
+        print("2. List network interfaces")
+        print("3. Quit")
+
+        option = int(input("Enter option number: "))
+
+        if option == 1:
+            interface = input("Enter network interface: ")
+            state = input("Turn on (on) or off (off) monitor mode?\n")
+            MonitorMode(interface, state)
+        elif option == 2:
+            ListInterfaces()
+        elif option == 3:
+            print("Quitting...")
+            break
+        else:
+            print("Invalid option, try again")
+
+if __name__ == "__main__":
+    main()
